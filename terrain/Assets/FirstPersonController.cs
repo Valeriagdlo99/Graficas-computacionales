e6eb59b2a68e0e3138105231a5109032938b451b
mariana394 <61065682+mariana394@users.noreply.github.com>
2022-11-30 15:28:51 -0600

    Musica y detalles finales
    Musica y detalles finales

--- terrain/Assets/FirstPersonController.cs
+++ terrain/Assets/FirstPersonController.cs
@@ -6,19 +6,18 @@ public class FirstPersonController : MonoBehaviour
 {
    CharacterController characterController;
 
-    [Header("Opciones Personaje")]
     public float walkSpeed= 6.0f;
     public float runSpeed= 10.0f;
     public float jumpSpeed= 8.0f;
     public float gravity= 20.0f;
 
-    [Header("Opciones Camara")]
     public Camera cam;
     public float mouseHorizontal = 3.0f;
     public float mouseVertical  =2.0f;
     public float minRotation = -65.0f;
     public float maxRotation = 60.0f;
     float h_mouse, v_mouse;
+
 
     private Vector3 move = Vector3.zero;
     // Start is called before the first frame update
@@ -32,19 +31,22 @@ public class FirstPersonController : MonoBehaviour
     void Update()
     {
 
-         h_mouse = mouseHorizontal * Input.GetAxis("Mouse X");
-         v_mouse += mouseVertical * Input.GetAxis("Mouse Y");
+        h_mouse = mouseHorizontal * Input.GetAxis("Mouse X");
+        v_mouse += mouseVertical * Input.GetAxis("Mouse Y");
 
-         v_mouse = Mathf.Clamp(v_mouse,minRotation,maxRotation);
-         cam.transform.localEulerAngles=new Vector3(-v_mouse,0,0);
+        v_mouse = Mathf.Clamp(v_mouse,minRotation,maxRotation);
+        cam.transform.localEulerAngles=new Vector3(-v_mouse,0,0);
          transform.Rotate(0, h_mouse, 0);
 
         if(characterController.isGrounded){
+            
             move =new Vector3(Input.GetAxis("Horizontal"),0.0f,Input.GetAxis("Vertical"));
+            
             if(Input.GetKey(KeyCode.LeftShift))
                 move= transform.TransformDirection(move) * runSpeed;
             else
                 move= transform.TransformDirection(move) * walkSpeed;
+            
             if(Input.GetKey(KeyCode.Space))
                 move.y = jumpSpeed;
             
